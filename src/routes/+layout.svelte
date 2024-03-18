dae476adc817fce936d8d64696e3f897073c5bbc
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-03-18 13:22:14 -0700

    basic styling of navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,58 @@
+<script>
+    import Linkedin from '$lib/icons/linkedin.svelte';
+    import Github from '$lib/icons/github.svelte';
+</script>
+
 <nav>
-    <a href="/">tig-blog</a>
-    <a href="https://tig-github.github.io/" target=”_blank”>tig-portfolio</a>
-    <a href="https://www.linkedin.com/in/rayantig/" target=”_blank”>linkedin</a>
-    <a href="https://github.com/tig-github" target=”_blank”>github</a>
+    <a href="/" id="blogTitle">tig-blog</a>
+    <div class="linkGroup">
+        <a href="https://tig-github.github.io/" target=”_blank” id="portfolioLink">tig-portfolio</a>
+        <a href="https://www.linkedin.com/in/rayantig/" target=”_blank” class="icon">
+            <Linkedin />
+        </a>
+        <a href="https://github.com/tig-github" target=”_blank” class="icon">
+            <Github />
+        </a>
+    </div>
 </nav>
 <body>
-    <slot />
+    <div class="content">
+        <slot />
+    </div>
 </body>
 
 <style>
     nav {
         background-color: #f8deff;
         padding: 2rem;
+        display: flex;
+        gap: 2rem;
+        justify-content: space-around;
     }
-    body {
-        background-color: #fff2fb;
+
+    div.linkGroup {
+        display: flex;
+        gap: 2rem;
+        white-space: nowrap;
+    }
+
+    #blogTitle {
+        width: 75%;
+        font-size: 2.5rem;
+        font-weight: bold;
+    }
+
+    #portfolioLink {
+        margin-top: 1rem;
+        font-size: 1.25rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    .content {
+        margin: 2rem;
     }
 </style>
