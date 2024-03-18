d02654b4e0a5e0ae4775cd4a081301f6fbe768ef
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-03-18 12:46:07 -0700

    basic setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<nav>
+    <a href="/">tig-blog</a>
+    <a href="https://tig-github.github.io/" target=”_blank”>tig-portfolio</a>
+    <a href="https://www.linkedin.com/in/rayantig/" target=”_blank”>linkedin</a>
+    <a href="https://github.com/tig-github" target=”_blank”>github</a>
+</nav>
+<body>
+    <slot />
+</body>
+
+<style>
+    nav {
+        background-color: #f8deff;
+        padding: 2rem;
+    }
+    body {
+        background-color: #fff2fb;
+    }
+</style>
