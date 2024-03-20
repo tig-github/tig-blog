02e7ca8b37d775e55f726bdd72b1aa870de7ab73
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-03-19 23:50:18 -0700

    more basic layout and new svg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
     import Linkedin from '$lib/icons/linkedin.svelte';
     import Github from '$lib/icons/github.svelte';
+    import Yaz from '$lib/icons/yaz.svelte';
 </script>
 
 <nav>
     <a href="/" id="blogTitle">tig-blog</a>
     <div class="linkGroup">
-        <a href="https://tig-github.github.io/" target=”_blank” id="portfolioLink">tig-portfolio</a>
+        <a href="https://tig-github.github.io/" target=”_blank” id="yazIcon">
+            <Yaz />
+        </a>
         <a href="https://www.linkedin.com/in/rayantig/" target=”_blank” class="icon">
             <Linkedin />
         </a>
@@ -16,18 +19,53 @@
     </div>
 </nav>
 <body>
-    <div class="content">
-        <slot />
+    <div class="bodyLayout">
+        <div class="content">
+            <slot />
+        </div>
+        <div class="sidebar">
+            <a href="/post1">
+                demo article 1
+            </a>
+            <a href="/post2">
+                demo article 2
+            </a>
+            <a href="/post3">
+                demo article 3
+            </a>
+        </div>
     </div>
 </body>
 
 <style>
     nav {
-        background-color: #f8deff;
+        background-color: #f7e5fc;
         padding: 2rem;
         display: flex;
         gap: 2rem;
         justify-content: space-around;
+    }
+
+    div.bodyLayout {
+        display: flex;
+        flex-direction: row;
+    }
+
+    div.content {
+        display: flex;
+        flex-direction: column;
+        width: 80%;
+        justify-content: center;
+        text-align: center;
+
+    }
+
+    div.sidebar {
+        display: flex;
+        flex-direction: column;
+        width: 20%;
+        gap: 2rem;
+        margin-top: 3.5rem;
     }
 
     div.linkGroup {
@@ -42,9 +80,8 @@
         font-weight: bold;
     }
 
-    #portfolioLink {
-        margin-top: 1rem;
-        font-size: 1.25rem;
+    #yazIcon {
+        margin-bottom: 0rem;
     }
 
     a {
